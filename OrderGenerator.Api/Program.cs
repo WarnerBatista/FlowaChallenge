b0b743bd582aa13bbdaff32da44392002350d405
Warner <warnerbatista@gmail.com>
2026-06-16 16:01:05 -0300

    Implementado a base para comunicação do OrderGenerator

--- OrderGenerator.Api/Program.cs
+++ OrderGenerator.Api/Program.cs
@@ -1,12 +1,19 @@
+using OrderGenerator.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton(new FixInitiatorService("config/initiator.cfg"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+var fixService = app.Services.GetRequiredService<FixInitiatorService>();
+fixService.Start();
 
 app.UseHttpsRedirection();
 
