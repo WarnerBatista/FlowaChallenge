f40f79eca380d762f8e657199a043d1af3deccb2
Warner <warnerbatista@gmail.com>
2026-06-17 17:48:44 -0300

    Ajustado o OrderAccumulatorApplication para retornar o orderId correto

--- OrderAccumulator/Services/OrderAccumulatorApplication.cs
+++ OrderAccumulator/Services/OrderAccumulatorApplication.cs
@@ -1,36 +1,33 @@
 ﻿using QuickFix;
 using QuickFix.Fields;
-using QuickFix.FIX44;
 
 namespace OrderAccumulator.Services;
 
 public class OrderAccumulatorApplication : IApplication
 {
-    int orderID = 0;
     int execID = 0;
 
-    private string GenOrderID() { return (++orderID).ToString(); }
     private string GenExecID() { return (++execID).ToString(); }
 
     public void OnCreate(SessionID sessionID) => Console.WriteLine($"Sessão criada: {sessionID}");
     public void OnLogon(SessionID sessionID) => Console.WriteLine($"Logon: {sessionID}");
     public void OnLogout(SessionID sessionID) => Console.WriteLine($"Logout: {sessionID}");
-    public void ToAdmin(QuickFix.Message message, SessionID sessionID) { }
-    public void FromAdmin(QuickFix.Message message, SessionID sessionID) { }
-    public void ToApp(QuickFix.Message message, SessionID sessionID) => Console.WriteLine($"Enviando mensagem: {message}");
+    public void ToAdmin(Message message, SessionID sessionID) { }
+    public void FromAdmin(Message message, SessionID sessionID) { }
+    public void ToApp(Message message, SessionID sessionID) => Console.WriteLine($"Enviando mensagem: {message}");
 
-    public void FromApp(QuickFix.Message message, SessionID sessionID)
+    public void FromApp(Message message, SessionID sessionID)
     {
         Console.WriteLine($"Mensagem recebida: {message}");
 
-        if (message is NewOrderSingle order)
+        if (message is QuickFix.FIX44.NewOrderSingle order)
         {
             Console.WriteLine("Nova ordem recebida.");
 
             // Todo: Aplicar lógica de acumulação de ordens aqui, por enquanto apenas respondemos com um ExecutionReport do tipo New.
 
-            var execReport = new ExecutionReport(
-                new OrderID(GenOrderID()),
+            var execReport = new QuickFix.FIX44.ExecutionReport(
+                new OrderID(order.ClOrdID.Value),
                 new ExecID(GenExecID()),
                 new ExecType(ExecType.NEW),
                 new OrdStatus(OrdStatus.NEW),
