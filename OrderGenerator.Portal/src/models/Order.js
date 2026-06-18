ff147841e4f68f272694385d1f4849b0c3c4d069
Warner <warnerbatista@gmail.com>
2026-06-18 10:40:45 -0300

    Removido model não usado

--- OrderGenerator.Portal/src/models/Order.js
+++
@@ -1,8 +0,0 @@
-export default class Order {
-  constructor(symbol, side, quantity, price) {
-    this.symbol = symbol;
-    this.side = side;
-    this.quantity = quantity;
-    this.price = price;
-  }
-}
