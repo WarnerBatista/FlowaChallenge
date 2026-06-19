bdd712984fe9f4d74394c18bb9f59cdf5c28b798
Warner <warnerbatista@gmail.com>
2026-06-19 17:35:40 -0300

    Criado testes para o ExposureAccumulator

---
+++ OrderAccumulator.UnitTests/Services/ExposureAccumulatorTests.cs
@@ -0,0 +1,66 @@
+﻿using OrderAccumulator.Services;
+
+namespace OrderAccumulator.UnitTests.Services;
+
+public class ExposureAccumulatorTests
+{
+    [Fact]
+    public void GetExposure_ShouldReturnCurrentExposure_ForGivenSymbol()
+    {
+        // Arrange
+        var sut = new ExposureAccumulator();
+        string symbol = "PETR4";
+        decimal expectedExposure = 5000m;
+        sut.SetExposure(symbol, expectedExposure);
+
+        // Act
+        var actualExposure = sut.GetExposure(symbol);
+
+        // Assert
+        Assert.Equal(expectedExposure, actualExposure);
+    }
+
+    [Fact]
+    public void IsLessThanMaxExposure_ShouldReturnTrue_WhenExposureIsBelowMax()
+    {
+        // Arrange
+        var sut = new ExposureAccumulator();
+        decimal exposure = 5000m;
+
+        // Act
+        var result = sut.IsLessThanMaxExposure(exposure);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsLessThanMaxExposure_ShouldReturnFalse_WhenExposureIsAboveMax()
+    {
+        // Arrange
+        var sut = new ExposureAccumulator();
+        decimal exposure = 150000000m;
+
+        // Act
+        var result = sut.IsLessThanMaxExposure(exposure);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void SetExposure_ShouldUpdateExposure_ForGivenSymbol()
+    {
+        // Arrange
+        var sut = new ExposureAccumulator();
+        string symbol = "PETR4";
+        decimal newExposure = 7500m;
+
+        // Act
+        sut.SetExposure(symbol, newExposure);
+        var actualExposure = sut.GetExposure(symbol);
+
+        // Assert
+        Assert.Equal(newExposure, actualExposure);
+    }
+}
