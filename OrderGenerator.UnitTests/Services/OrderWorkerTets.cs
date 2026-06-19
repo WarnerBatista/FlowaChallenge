9d614e2760540de4a9acb842b0be3cc773d9b0ce
Warner <warnerbatista@gmail.com>
2026-06-19 20:37:26 -0300

    Criado testes para o OrderWorker

---
+++ OrderGenerator.UnitTests/Services/OrderWorkerTets.cs
@@ -0,0 +1,22 @@
+﻿using NSubstitute;
+using OrderGenerator.Api.Services;
+using OrderGenerator.Api.Services.Interfaces;
+
+namespace OrderGenerator.UnitTests.Services;
+
+public class OrderWorkerTets
+{
+    [Fact]
+    public void Constructor_Throws_When_OrderQueue_Is_Null()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () => new OrderWorker(null!, Substitute.For<ISessionIdProvider>()));
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_SessionIdProvider_Is_Null()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () => new OrderWorker(Substitute.For<IOrderQueue>(), null!));
+    }
+}
