d1ad7ae9dd1d99a73c7337457c5dadc3106f927e
Warner <warnerbatista@gmail.com>
2026-06-19 16:17:32 -0300

    Criado testes unitários para o NewOrderSingleExtensions

---
+++ OrderAccumulator.UnitTests/Attributes/AutoNSubstituteAttribute.cs
@@ -0,0 +1,13 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+
+namespace OrderAccumulator.UnitTests.Attributes;
+
+public class AutoNSubstituteAttribute : AutoDataAttribute
+{
+    public AutoNSubstituteAttribute()
+        : base(() => new Fixture().Customize(new AutoNSubstituteCustomization()))
+    {
+    }
+}
